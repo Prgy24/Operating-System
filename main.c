63842aaa9e0eb38e176ea2febbbe99127d5664d7
Prgy24 <39917177+Prgy24@users.noreply.github.com>
2019-01-26 19:33:26 +0530

    MAKEFILE

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 {
 
 	printf("\nadd: %d\n",add(10,27));
-	//printf("\nsub: %d\n",sub(10,27));
-	printf("\nadd1: %d\n",add(10,7));
+	printf("\nsub: %d\n",sub(10,27));
+	//printf("\nadd1: %d\n",add(10,7));
 	return 0;
 }
